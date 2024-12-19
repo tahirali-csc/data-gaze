ba0cd10ea9db77429ddc17f4ea085391f70adca2
tahir <tahirali.csc@gmail.com>
2024-12-19 16:34:46 -0600

    add setup ebpf script

---
+++ ebpf/setup-ebpf.sh
@@ -0,0 +1,12 @@
+# Installs necessary ebpf related dependencies
+sudo apt update
+sudo apt install clang -y
+
+sudo apt install linux-headers-$(uname -r) -y
+sudo apt install libbpf-dev
+
+# hack for this error
+# usr/include/linux/types.h:5:10: fatal error: 'asm/types.h' file not found 
+# https://stackoverflow.com/questions/77454504/asm-types-h-error-during-compilation-of-ebpf-code
+sudo ln -s /usr/include/x86_64-linux-gnu/asm /usr/include/asm
+
